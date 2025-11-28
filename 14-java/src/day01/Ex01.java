d5319b77b1c3dc78a9f66f07769e9c53e5f1c12a
Kim, Jeongin <twkim721@naver.com>
2025-11-28 17:57:56 +0900

    day01-update

---
+++ 14-java/src/day01/Ex01.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Ex01 {
+    static void main(String[] args) {
+        int value = 100;
+        double factor = 0.98;
+        char grade = 'A';
+
+        System.out.println("Value: " + value + ", Factor: " + factor + ", Grade: " + grade);
+
+        System.out.printf("Value: %d, Factor: %.2f, Grade: %c\n", value, factor, grade);
+
+    }
+}
---
+++ 14-java/src/day01/Pratice.java
@@ -0,0 +1,16 @@
+package day01;
+
+import java.sql.SQLOutput;
+
+public class Pratice {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+        System.out.printf("x: %d, y: %d\n", x, y);
+        System.out.printf("x + y = %d\n", x + y);
+        System.out.printf("x - y = %d\n", x - y);
+        System.out.printf("x * y = %d\n", x * y);
+        System.out.printf("x / y = %.2f\n", (double)x / y);
+        System.out.printf("x %% y = %d\n", x % y);
+    }
+}
